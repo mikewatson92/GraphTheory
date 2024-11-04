cc13e5eec264aadc0d9e36565f319df0eef1d25e
Mike Watson <mikewatson92@icloud.com>
2024-11-04 10:42:04 -0600

    Initial commit

--- GraphTheory/Algorithms/Permutation.swift
+++ GraphTheory/Algorithms/Permutation.swift
@@ -7,46 +7,25 @@
 
 import Foundation
 
-public class Permutation {
+struct Permutation<T> {
     
-    static func permuteVertices(_ values: [Vertex]) -> [[Vertex]]{
-        var values = values
-        var newValues = values
-        var permutations: [[Vertex]] = []
+    static func permute(_ list: [T]) -> [[T]] {
+        var permutations: [[T]] = []
+        var listToPermute = list
         
-        func swap(_ pair: [Vertex]) -> [Vertex] {
-            var pair = pair
-            let tempA = pair[0]
-            pair[0] = pair[1]
-            pair[1] = tempA
-            return pair
+        if list.count == 1 {
+            return [list]
         }
         
-        func swap(_ first: Int, _ second: Int) -> [Vertex] {
-            let tempFirst = values[first]
-            var newValues = values
-            newValues[first] = newValues[second]
-            newValues[second] = tempFirst
-            return newValues
-        }
-        
-        if values.count == 2 {
-            permutations.append(values)
-            values = swap(values)
-            permutations.append(values)
-            return permutations
-        } else {
-            for i in 0..<values.count {
-                newValues = swap(0, i)
-                let shortenedValues = newValues.suffix(from: 1)
-                let shortPermutations = permuteVertices(Array(shortenedValues))
-                for shortPermuted in shortPermutations {
-                    var temp = shortPermuted
-                    temp.insert(newValues[0], at: 0)
-                    permutations.append(temp)
-                }
+        for i in 0..<list.count {
+            let removedElement: T = listToPermute.remove(at: i)
+            var remainingPermuted = permute(listToPermute)
+            for j in 0..<remainingPermuted.count {
+                remainingPermuted[j].insert(removedElement, at: 0)
+                permutations.append(remainingPermuted[j])
             }
-            return permutations
+            listToPermute = list
         }
+        return permutations
     }
 }
